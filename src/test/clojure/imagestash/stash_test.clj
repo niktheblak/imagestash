dff2b7fc4db6a11bb1eebb6cc35671e60772863e
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-09-04 16:02:39 +0300

    Fixed imagestash.stash-test unit tests

--- src/test/clojure/imagestash/stash_test.clj
+++ src/test/clojure/imagestash/stash_test.clj
@@ -1,8 +1,8 @@
-(ns imagestash.image-io-test
+(ns imagestash.stash-test
   (:import [java.io File]
            [java.util Arrays Random])
   (:require [clojure.test :refer :all]
-            [imagestash.stash :as image]))
+            [imagestash.stash :as stash]))
 
 (defn- temp-file []
   (let [file (File/createTempFile "file-test-" ".tmp")
@@ -20,19 +20,19 @@
 
 (def test-image {:key "test-key"
                  :size 1024
-                 :format "JPEG"
+                 :format :jpeg
                  :data test-image-data})
 
 (deftest write-test
   (let [file (temp-file)]
     (testing "write to an empty file"
-      (let [{:keys [size-on-disk]} (image/write-to file test-image)
+      (let [{:keys [size-on-disk]} (stash/write-to file test-image)
             file-len (.length file)]
         (is (divisible-by-eight size-on-disk))
         (is (= 152 size-on-disk))
         (is (= size-on-disk file-len))))
     (testing "append to a non-empty file"
-      (let [{:keys [size-on-disk]} (image/write-to file (assoc test-image :key "test-key-2"))
+      (let [{:keys [size-on-disk]} (stash/write-to file (assoc test-image :key "test-key-2"))
             file-len (.length file)]
         (is (= 160 size-on-disk))
         (is (= 312 file-len))))))
@@ -40,23 +40,23 @@
 (deftest read-test
   (let [file (temp-file)]
     (testing "write/read roundtrip"
-      (let [{:keys [size-on-disk]} (image/write-to file test-image)
-            read-image (image/read-from file 0)]
+      (let [{:keys [size-on-disk]} (stash/write-to file test-image)
+            read-image (stash/read-from file 0)]
         (is (= "test-key" (:key read-image)))
         (is (= 1024 (:size read-image)))
-        (is (= "JPEG" (:format read-image)))
+        (is (= :jpeg (:format read-image)))
         (is (Arrays/equals test-image-data (:data read-image)))
         (is (= size-on-disk (:stored-length read-image)))))
     (testing "read should throw on invalid header"
-      (is (thrown-with-msg? RuntimeException #"Invalid header" (image/read-from file 3))))))
+      (is (thrown-with-msg? RuntimeException #"Invalid header" (stash/read-from file 3))))))
 
 (deftest size-on-disk-test
   (let [file (temp-file)]
     (testing "size-on-disk"
-      (let [expected-size (image/size-on-disk test-image)
-            {:keys [size-on-disk]} (image/write-to file test-image)
+      (let [expected-size (stash/size-on-disk test-image)
+            {:keys [size-on-disk]} (stash/write-to file test-image)
             file-size (.length file)
-            read-image (image/read-from file 0)]
+            read-image (stash/read-from file 0)]
         (is (= expected-size size-on-disk))
         (is (= expected-size file-size))
         (is (= expected-size (:stored-length read-image)))))))

58314a7b280f95e83e46c5a61c0ee13dfb0ac51b
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-09-05 12:16:24 +0300

    Added unit test for imagestash.resize

---
+++ src/test/clojure/imagestash/resize_test.clj
@@ -0,0 +1,15 @@
+(ns imagestash.resize-test
+  (:import [java.io File ByteArrayInputStream]
+           [javax.imageio ImageIO]
+           [java.awt.image BufferedImage])
+  (:require [clojure.test :refer :all]
+            [imagestash.resize :as resize]))
+
+(def source-image (File. "src/test/resources/ABC_FlockFav_2014_1024x500_1.jpg"))
+
+(deftest resize-test
+  (testing "resize"
+    (let [resized-data (resize/resize-image source-image 600 :jpeg)]
+      (resize/with-image [resized-image (ImageIO/read (ByteArrayInputStream. resized-data))]
+        (is (= 600 (.getWidth resized-image)))
+        (is (= 293 (.getHeight resized-image)))))))

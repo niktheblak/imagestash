675755a61d7d52e21215fa1e53721710bace8ffb
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-09-23 13:37:40 +0300

    Moved digest functionality from imagestash.io to imagestash.digest

--- src/main/clojure/imagestash/digest.clj
+++ src/main/clojure/imagestash/digest.clj
@@ -1,5 +1,6 @@
 (ns imagestash.digest
-  (:import [java.security MessageDigest])
+  (:import [java.security MessageDigest]
+           [java.io DataInput DataOutput])
   (:require [imagestash.io :as io]))
 
 (defn get-digest []
@@ -7,12 +8,36 @@
 
 (def digest-length (.getDigestLength (get-digest)))
 
+(defn update-digest-int [^MessageDigest digest n]
+  (let [b4 (bit-and n 0xFF)
+        b3 (bit-and (bit-shift-right n 8) 0xFF)
+        b2 (bit-and (bit-shift-right n 16) 0xFF)
+        b1 (bit-and (bit-shift-right n 24) 0xFF)]
+    (doto digest
+      (.update (unchecked-byte b1))
+      (.update (unchecked-byte b2))
+      (.update (unchecked-byte b3))
+      (.update (unchecked-byte b4)))))
+
+(defn update-digest [^MessageDigest digest data]
+  (cond
+    (and (integer? data) (<= data 255)) (.update digest (unchecked-byte data))
+    (integer? data) (update-digest-int digest data)
+    (io/byte-array? data) (.update digest data)
+    :else (.update digest (io/to-bytes data))))
+
+(defn read-and-digest [^DataInput input ^MessageDigest digest read-fn]
+  (let [data (read-fn input)]
+    (update-digest digest data)
+    data))
+
+(defn write-and-digest [^DataOutput output ^MessageDigest digest data write-fn]
+  (write-fn output data)
+  (update-digest digest data))
+
 (defn digest [& items]
   {:post [(io/byte-array? %)]}
   (let [digest (get-digest)]
     (doseq [n items]
-      (cond
-        (and (integer? n) (< n 256)) (.update digest (unchecked-byte n))
-        (integer? n) (io/update-digest-int digest n)
-        :else (.update digest (io/to-bytes n))))
+      (update-digest digest n))
     (.digest digest)))
--- src/main/clojure/imagestash/io.clj
+++ src/main/clojure/imagestash/io.clj
@@ -2,8 +2,7 @@
   (:import [java.io InputStream DataInput DataOutput]
            [java.util Arrays]
            [java.nio.charset Charset]
-           [java.nio ByteBuffer]
-           [java.security MessageDigest]))
+           [java.nio ByteBuffer]))
 
 (def default-charset (Charset/forName "UTF-8"))
 
@@ -16,17 +15,6 @@
 (defn str-to-bytes [str]
   {:pre [(string? str)]}
   (.getBytes str default-charset))
-
-(defn update-digest-int [^MessageDigest digest n]
-  (let [b4 (bit-and n 0xFF)
-        b3 (bit-and (bit-shift-right n 8) 0xFF)
-        b2 (bit-and (bit-shift-right n 16) 0xFF)
-        b1 (bit-and (bit-shift-right n 24) 0xFF)]
-    (doto digest
-      (.update (unchecked-byte b1))
-      (.update (unchecked-byte b2))
-      (.update (unchecked-byte b3))
-      (.update (unchecked-byte b4)))))
 
 (defn to-bytes [n]
   {:post [(byte-array? %)]}
@@ -73,19 +61,3 @@
 
 (defn write-int [^DataOutput output n]
   (.writeInt output n))
-
-(defn update-digest [^MessageDigest digest data]
-  (cond
-    (instance? Byte data) (.update digest data)
-    (integer? data) (update-digest-int digest data)
-    (byte-array? data) (.update digest data)
-    :else (throw (ex-info "Unknown data format" {:data data}))))
-
-(defn read-and-digest [^DataInput input ^MessageDigest digest read-fn]
-  (let [data (read-fn input)]
-    (update-digest digest data)
-    data))
-
-(defn write-and-digest [^DataOutput output ^MessageDigest digest data write-fn]
-  (write-fn output data)
-  (update-digest digest data))

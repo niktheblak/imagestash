d6bedcf0f603e6e0b54dbdc3e08ab71e63f78825
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-11-19 16:24:59 +0200

    Use compojure instead of ring in service.clj

--- project.clj
+++ project.clj
@@ -4,12 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [org.imgscalr/imgscalr-lib "4.2"]]
+                 [org.imgscalr/imgscalr-lib "4.2"]
+                 [compojure "1.2.1"]
+                 [ring/ring-defaults "0.1.2"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"]
   :target-path "target/%s"
-  :main ^:skip-aot imagestash.service
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler imagestash.service/app
+         :port 8080
+         :init imagestash.service/start
+         :destroy imagestash.service/stop})
--- src/main/clojure/imagestash/service.clj
+++ src/main/clojure/imagestash/service.clj
@@ -3,58 +3,54 @@
   (:require [imagestash.broker :as br]
             [imagestash.format :as fmt]
             [imagestash.index :as idx]
-            [ring.adapter.jetty :as jet]
-            [ring.util.response :refer :all]
-            [ring.middleware.params :as params]))
+            [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [ring.util.response :as response]
+            [ring.middleware.defaults :as rmd]))
 
 (def index-file-name "index.bin")
 
-(defn- load-index []
-  (let [file (File. index-file-name)]
+(def broker (ref nil))
+
+(defn load-index [file-name]
+  (let [file (File. file-name)]
     (if (.exists file)
         (let [index (idx/load-index file)]
           (println "Loaded" (count index) "images from index file" index-file-name)
           index)
       {})))
 
-(defn accept-resize-route [handler]
-  (fn [request]
-    (let [path (:uri request)]
-      (if (= "/resize" path)
-        (if (and
-              (get-in request [:params "source"])
-              (get-in request [:params "size"]))
-          (handler request)
-          {:status 400
-           :headers {"Content-Type" "text/plain"}
-           :body   "Invalid parameters"})
-        (not-found (str "Path" path " not found"))))))
+(defn start []
+  (let [index (load-index index-file-name)
+        br (br/create-broker 1 :index index)]
+    (dosync
+      (ref-set broker br)))
+  (println "Started imagestash server"))
 
-(defn handler [broker request]
-  (let [source (get-in request [:params "source"])
-        raw-size (get-in request [:params "size"])
-        raw-format (get-in request [:params "format"] "jpeg")
-        size (Integer/parseInt raw-size)
-        format (fmt/parse-format raw-format)
+(defn stop []
+  (println "Saving index with" (count (:index @broker)) "images to file" index-file-name)
+  (idx/save-index index-file-name (:index @broker))
+  (shutdown-agents)
+  (println "System shutdown completed"))
+
+
+(defn resize-handler [source raw-size raw-format]
+  (let [size (Integer/parseInt raw-size)
+        format (if raw-format
+                 (fmt/parse-format raw-format)
+                 :jpeg)
         image-source (br/from-internet-source source size :format format)]
-    (if-let [image (br/get-or-add-image broker image-source)]
+    (if-let [image (br/get-or-add-image @broker image-source)]
       (let [length (alength (:data image))
             stream (ByteArrayInputStream. (:data image))]
         {:status  200
          :headers {"Content-Type"   (fmt/format-mime-type (:format image))
                    "Content-Length" (str length)}
          :body    stream})
-      (not-found (str "Image with source " source " was not found")))))
+      (response/not-found (str "Image with source " source " was not found")))))
 
-(defn -main []
-  (let [broker (br/create-broker 1 :index (load-index))
-        handler (accept-resize-route (partial handler broker))
-        server (jet/run-jetty (params/wrap-params handler) {:port 8080 :join? false})]
-    (println "Started imagestash server. Press <ENTER> to quit...")
-    (read-line)
-    (println "Stopping imagestash server")
-    (.stop server)
-    (println "Saving index with" (count (:index @broker)) "images to file" index-file-name)
-    (idx/save-index index-file-name (:index @broker))
-    (shutdown-agents)
-    (println "System shutdown completed")))
+(defroutes resize-routes
+  (GET "/resize" [source size format] (resize-handler source size format)))
+
+(def app
+  (handler/site (rmd/wrap-defaults resize-routes rmd/api-defaults)))

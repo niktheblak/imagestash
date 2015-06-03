6e5c1ac1e0f9c34892a3712699d05b10989ef6d2
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2015-06-03 10:03:16 +0300

    Updated 3rd party dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.imgscalr/imgscalr-lib "4.2"]
-                 [compojure "1.3.3"]
-                 [ring/ring-defaults "0.1.4"]
+                 [compojure "1.3.4"]
+                 [ring/ring-defaults "0.1.5"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ring/ring-mock "0.2.0"]]
   :jvm-opts ["-Djava.awt.headless=true"]
@@ -14,7 +14,7 @@
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"]
   :target-path "target/%s"
-  :plugins [[lein-ring "0.9.3"]]
+  :plugins [[lein-ring "0.9.4"]]
   :ring {:handler imagestash.service/app
          :port 8080
          :init imagestash.service/start

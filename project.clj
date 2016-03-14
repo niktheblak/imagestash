8dab82679d6587a02cea854663b67f719f5e1003
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2016-03-14 13:54:38 +0200

    Updated to compojure-api 1.0.1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.imgscalr/imgscalr-lib "4.2"]
-                 [metosin/compojure-api "1.0.0-RC1"]
+                 [metosin/compojure-api "1.0.1"]
                  [ring/ring-mock "0.3.0"]
                  [clj-time "0.11.0"]
                  [org.clojure/tools.logging "0.3.1"]]
